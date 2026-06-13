82bc16f8c53fcc25bcb08d9eb1a6326ef4e6454a
VivianeMezzomo <viviane.mezzomo@codex.com.br>
2026-06-13 15:15:43 -0300

    Criando conexão com o banco e startando o server

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import app from "./src/app.js";
+import dotenv from "dotenv";
+import connectDatabase from "./infrastructure/database/connection.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+async function startServer() {
+  await connectDatabase();
+
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+}
+
+startServer();
